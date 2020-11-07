d95065ee803823ae84b60f15eb8d0e1722f7f168
sb6495 <74091335+sb6495@users.noreply.github.com>
2020-11-07 15:57:37 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,17 +1,17 @@
 KSB045.onBtnChanged(KSB045.btnName.C, KSB045.pushType.down, function () {
-    radio.sendString("c")
+    radio.sendString("11")
 })
 KSB045.onBtnChanged(KSB045.btnName.D, KSB045.pushType.down, function () {
-    radio.sendString("d")
+    radio.sendString("12")
 })
 KSB045.onBtnChanged(KSB045.btnName.SW, KSB045.pushType.down, function () {
     radio.sendNumber(0)
 })
 KSB045.onBtnChanged(KSB045.btnName.F, KSB045.pushType.down, function () {
-    radio.sendString("f")
+    radio.sendString("14")
 })
 KSB045.onBtnChanged(KSB045.btnName.E, KSB045.pushType.down, function () {
-    radio.sendString("e")
+    radio.sendString("13")
 })
 radio.setGroup(20)
 radio.setTransmitPower(7)
@@ -26,7 +26,7 @@ basic.forever(function () {
             . . # . .
             `)
     }
-    if (KSB045.getJoystickValue(KSB045.valueType.X) <= 250 && KSB045.getJoystickValue(KSB045.valueType.X) > 450) {
+    if (KSB045.getJoystickValue(KSB045.valueType.X) > 250 && KSB045.getJoystickValue(KSB045.valueType.X) > 450) {
         radio.sendNumber(2)
         basic.showLeds(`
             . . # . .
@@ -36,7 +36,7 @@ basic.forever(function () {
             . . # . .
             `)
     }
-    if (KSB045.getJoystickValue(KSB045.valueType.X) <= 550 && KSB045.getJoystickValue(KSB045.valueType.X) > 750) {
+    if (KSB045.getJoystickValue(KSB045.valueType.X) > 550 && KSB045.getJoystickValue(KSB045.valueType.X) > 750) {
         radio.sendNumber(3)
         basic.showLeds(`
             . . # . .
@@ -59,42 +59,42 @@ basic.forever(function () {
 })
 basic.forever(function () {
     if (KSB045.getJoystickValue(KSB045.valueType.Y) <= 250) {
-        radio.sendNumber(1)
+        radio.sendNumber(5)
         basic.showLeds(`
             . . # . .
-            . . # . .
-            # . # . #
             . # # # .
-            . . # . .
-            `)
-    }
-    if (KSB045.getJoystickValue(KSB045.valueType.Y) <= 250 && KSB045.getJoystickValue(KSB045.valueType.Y) > 450) {
-        radio.sendNumber(2)
-        basic.showLeds(`
-            . . # . .
-            . . . . .
             # . # . #
-            . # . # .
+            . . # . .
             . . # . .
             `)
     }
-    if (KSB045.getJoystickValue(KSB045.valueType.Y) <= 550 && KSB045.getJoystickValue(KSB045.valueType.Y) > 750) {
-        radio.sendNumber(3)
+    if (KSB045.getJoystickValue(KSB045.valueType.Y) > 250 && KSB045.getJoystickValue(KSB045.valueType.Y) > 450) {
+        radio.sendNumber(6)
         basic.showLeds(`
             . . # . .
             . # . # .
             # . # . #
             . . . . .
+            . . # . .
+            `)
+    }
+    if (KSB045.getJoystickValue(KSB045.valueType.Y) > 550 && KSB045.getJoystickValue(KSB045.valueType.Y) > 750) {
+        radio.sendNumber(7)
+        basic.showLeds(`
+            . . # . .
+            . . . . .
+            # . # . #
+            . # . # .
             . . # . .
             `)
     }
     if (KSB045.getJoystickValue(KSB045.valueType.Y) <= 750) {
-        radio.sendNumber(4)
+        radio.sendNumber(8)
         basic.showLeds(`
             . . # . .
-            . # # # .
-            # . # . #
             . . # . .
+            # . # . #
+            . # # # .
             . . # . .
             `)
     }
